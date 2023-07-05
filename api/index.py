6bd45744beba4bbc44b11ba362ee43e02400a484
noseon <fake.on@gmail.com>
2023-07-05 00:21:53 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,1 @@
-from flask import Flask
-from flask_telebot import TeleBot
-from flask import Flask, render_template
 
-app = Flask(__name__)
-app.config['TELEBOT_TOKEN'] = '6294585980:AAH8WFD-CbbMfV6ZEtYV36m58qPGHvQTxjY'
-bot = TeleBot(app)
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.send_message(message.chat.id, 'Olá! Bem-vindo ao meu bot do Telegram.')
-
-if __name__ == '__main__':
-    bot.polling()
-    app.run()
